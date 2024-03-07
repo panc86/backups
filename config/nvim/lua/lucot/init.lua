9099eaeed240b001f491ea653f6bec81f5374696
Emanuele Panizio <panc86@gmail.com>
2024-03-07 16:35:03 +0100

    migrate discontinued packer plugin manager in favor of lazy

--- config/nvim/lua/lucot/init.lua
+++ config/nvim/lua/lucot/init.lua
@@ -1,2 +1,40 @@
 require("lucot.set")
 require("lucot.remap")
+require("lucot.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local LucotGroup = augroup('Lucot', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+
+autocmd('LspAttach', {
+    group = LucotGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    end
+})
