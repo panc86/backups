c81b96386cdf92ede96b8c0cdcaf7bc1558578e0
Emanuele Panizio <panc86@gmail.com>
2025-03-22 11:03:54 +0400

    update tmux and nvim config

--- config/nvim/lua/default/config/keymaps.lua
+++ config/nvim/lua/default/config/keymaps.lua
@@ -1,21 +1,30 @@
 -- Shorten function name
 local keymap = vim.keymap.set
 
--- Silent keymap option
-local opts = { silent = true, noremap = true }
+keymap("n", "<leader>.", vim.cmd.Ex, { silent = true, noremap = true, desc = "back to project view" })
 
--- back to project view
-keymap("n", "<leader>.", vim.cmd.Ex, opts)
+-- move text
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "move highlighted block down" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "move highlighted block up" })
 
--- move highlighted block down
-keymap("v", "J", ":m '>+1<CR>gv=gv")
-
--- move highlighted block up
-keymap("v", "K", ":m '<-2<CR>gv=gv")
-
--- keep last yank
-keymap("x", "<leader>p", [["_dP]])
-
+-- yank text
+keymap("x", "<leader>p", [["_dP]], { desc = "Delete and paste (keep clipboard)" })
 keymap({ "n", "v" }, "<leader>y", [["+y]])
-
 keymap("n", "<leader>Y", [["+Y]])
+
+-- Window navigation
+vim.keymap.set({ "n", "v", "i" }, "<C-h>", "<C-w>h", {
+	desc = "Navigate window left",
+})
+vim.keymap.set({ "n", "v", "i" }, "<C-j>", "<C-w>j", {
+	desc = "Navigate window down",
+})
+vim.keymap.set({ "n", "v", "i" }, "<C-k>", "<C-w>k", {
+	desc = "Navigate window up",
+})
+vim.keymap.set({ "n", "v", "i" }, "<C-l>", "<C-w>l", {
+	desc = "Navigate window right",
+})
+
+-- save changes
+vim.keymap.set("n", "<leader>w", "<cmd>update!<cr>", { desc = "Save" })
--- config/nvim/lua/default/lazy.lua
+++ config/nvim/lua/default/lazy.lua
@@ -12,11 +12,11 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "default.plugins" }, { import = "default.plugins.lsp" } }, {
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
