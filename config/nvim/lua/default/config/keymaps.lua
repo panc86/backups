6a146293f2d13fbdcd39438bfbd662b61aeea38c
Emanuele Panizio <panc86@gmail.com>
2025-04-07 08:38:14 +0400

    update ide windown navigation config

--- config/nvim/lua/default/config/keymaps.lua
+++ config/nvim/lua/default/config/keymaps.lua
@@ -2,11 +2,11 @@
 local keymap = vim.keymap.set
 
 -- explorer
-keymap("n", "<leader>.", vim.cmd.Ex, { silent = true, noremap = true, desc = "back to project view" })
+keymap("n", "<leader>.", vim.cmd.Ex, { silent = true, noremap = true, desc = "Go project view" })
 
 -- move text
-keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "move highlighted block down" })
-keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "move highlighted block up" })
+keymap("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move highlighted block down" })
+keymap("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted block up" })
 
 -- yank text
 keymap("x", "<leader>p", [["_dP]], { desc = "Delete and paste (keep clipboard)" })
@@ -14,18 +14,10 @@ keymap({ "n", "v" }, "<leader>y", [["+y]])
 keymap("n", "<leader>Y", [["+Y]])
 
 -- window navigation
-keymap({ "n", "v", "i" }, "<C-h>", "<C-w>h", {
-	desc = "Navigate window left",
-})
-keymap({ "n", "v", "i" }, "<C-j>", "<C-w>j", {
-	desc = "Navigate window down",
-})
-keymap({ "n", "v", "i" }, "<C-k>", "<C-w>k", {
-	desc = "Navigate window up",
-})
-keymap({ "n", "v", "i" }, "<C-l>", "<C-w>l", {
-	desc = "Navigate window right",
-})
+keymap({ "n", "v", "i" }, "<C-h>", "<C-w>h", { desc = "Go left window" })
+keymap({ "n", "v", "i" }, "<C-j>", "<C-w>j", { desc = "Go bottom window" })
+keymap({ "n", "v", "i" }, "<C-k>", "<C-w>k", { desc = "Go top window" })
+keymap({ "n", "v", "i" }, "<C-l>", "<C-w>l", { desc = "Go right window" })
 
 -- save changes
-keymap("n", "<leader>w", "<cmd>update!<cr>", { desc = "Save" })
+keymap("n", "<leader>w", "<cmd>update!<cr>", { desc = "Save buffer" })
